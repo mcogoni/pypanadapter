58fd229250acd975fe70172c61ff916989b1c3c7
Marco Cogoni <cogoni@gmail.com>
2019-08-15 12:58:20 +0200

    added red lines and khz limits text

--- pypanadapter.py
+++ pypanadapter.py
@@ -6,7 +6,7 @@ import pyqtgraph as pg
 import pyaudio
 from PyQt4 import QtCore, QtGui
 
-FS = 2.4e6 # Sampling Frequency of the RTL-SDR card (in Hz) # DON'T GO TOO LOW, QUALITY ISSUES ARISE
+FS = 1.0e6 # Sampling Frequency of the RTL-SDR card (in Hz) # DON'T GO TOO LOW, QUALITY ISSUES ARISE
 F_SDR = 8.8315e6 # center frequency in Hz # THIS IS FOR OLD KENWOOD RADIOS LIKE THE TS-180S (WIDE BAND IF OUTPUT)
 N_AVG = 1 # averaging over how many spectra
 
@@ -42,6 +42,13 @@ class SpectrogramWidget(pg.PlotWidget):
         self.N_WIN = 1024  # How many pixels to show from the FFT (around the center)
 
         self.img_array = 250*np.ones((self.N_WIN, self.N_WIN))
+        # Plot the grid
+        for x in [0, self.N_WIN/2, self.N_WIN-1]:
+            if x==0 or x==self.N_WIN-1:
+                self.img_array[:,x] = 0
+            else:
+                self.img_array[:,x] = 0
+
 
         # MATRIX Colormap
         pos = np.array([0., 0.5, 1.])
@@ -59,7 +66,19 @@ class SpectrogramWidget(pg.PlotWidget):
         self.waterfall.scale(self.bw_hz,1)
         self.setLabel('bottom', 'Frequency', units='kHz')
         
+        self.text_leftlim = pg.TextItem("%.1f kHz"%(-self.bw_hz*self.N_WIN/2.))
+        self.text_leftlim.setParentItem(self.waterfall)
+        self.plotwidget1.addItem(self.text_leftlim)
+        self.text_leftlim.setPos(0, 0)
+
+        self.text_rightlim = pg.TextItem("%.1f kHz"%(self.bw_hz*self.N_WIN/2.))
+        self.text_rightlim.setParentItem(self.waterfall)
+        self.plotwidget1.addItem(self.text_rightlim)
+        self.text_rightlim.setPos(self.bw_hz*(self.N_WIN-1), 0)
+
         self.plotwidget1.hideAxis("left")
+        self.plotwidget1.hideAxis("bottom")
+
         self.hideAxis("top")
         self.hideAxis("bottom")
         self.hideAxis("left")
@@ -90,13 +109,13 @@ class SpectrogramWidget(pg.PlotWidget):
 
     def on_increasebutton_clicked(self):
         self.N_FFT *= 2
-        self.waterfall.scale(0.5,1)
+        #self.waterfall.scale(0.5,1)
 
     
     def on_decreasebutton_clicked(self):
         self.N_FFT /= 2
-        self.waterfall.scale(2.0,1)
-
+        #self.waterfall.scale(2.0,1)
+ 
 
     def update(self, chunk):
         self.bw_hz = FS/float(self.N_FFT) * float(self.N_WIN)
@@ -104,17 +123,31 @@ class SpectrogramWidget(pg.PlotWidget):
 
         sample_freq, spec = welch(chunk, FS, window="hamming", nperseg=self.N_FFT,  nfft=self.N_FFT)
         spec = np.roll(spec, self.N_FFT/2, 0)[self.N_FFT/2-self.N_WIN/2:self.N_FFT/2+self.N_WIN/2]
-        #spec[self.N_WIN/2] = 255 # red line in the middle... a little buggy for USB/LSB!!! FIXME
+        
         # get magnitude 
         psd = abs(spec)
         # convert to dB scale
         psd = -20 * np.log10(psd)
+
+        # Plot the grid
+        for x in [0, self.N_WIN/2, self.N_WIN-1]:
+            if x==0 or x==self.N_WIN-1:
+                psd[x] = 0
+            else:
+                psd[x] = 0
+
         # roll down one and replace leading edge with new data
         self.img_array = np.roll(self.img_array, -1, 0)
         self.img_array[-1:] = psd
 
-        self.waterfall.setImage(self.img_array.T, autoLevels=False, opacity = 0.99)
-        
+        self.waterfall.setImage(self.img_array.T, autoLevels=False, opacity = 1.0, autoDownsample=True)
+
+        self.text_leftlim.setPos(0, 0)
+        self.text_leftlim.setText(text="%.1f kHz"%(-self.bw_hz/2000.))
+        #self.text_rightlim.setPos(self.bw_hz*1000, 0)
+        self.text_rightlim.setText(text="%.1f kHz"%(self.bw_hz/2000.))
+
+
 if __name__ == '__main__':
     app = QtGui.QApplication([])
     w = SpectrogramWidget()
